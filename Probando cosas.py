519deb02a5d43c3dd452267138fe71c80469f853
Cami Malatesta <camilamalatesta.exactas@gmail.com>
2020-02-28 00:51:18 -0300

    Ahora si lo puedo agregar al cv?

--- Probando cosas.py
+++ Probando cosas.py
@@ -1,45 +1,54 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 
-def clean(line: str) -> list:
-    return line.replace("\n","").replace(" ","").split(",")
+def open_csv(path: str) -> dict:
 
+    def _clean(line: str) -> list:
+        return line.replace("\n", "").replace(" ", "").split(",")
 
-def times(timestamp: str) -> int:
-    date_times = timestamp.split("-")
-    time = [int(x) for x in date_times[1].split(":")]
-    return time[0]*3600 + time[1]*60 + time[2]
+    def _times(timestamp: str) -> int:
+        date_times = timestamp.split("-")
+        time = [int(x) for x in date_times[1].split(":")]
+        return time[0]*3600 + time[1]*60 + time[2]
 
+    f = open(path, "r")
+    fields = _clean(f.readline())
 
-f = open("/home/camila/Descargas/vacas.csv", "r")
-fields = clean(f.readline())
+    _db = {field: list() for field in fields}
 
-db = {field: list() for field in fields}
-
-row = f.readline()
-while row != "":
-    row = clean(row)
-    for i in range(len(row)):
-        db[fields[i]].append(row[i])
     row = f.readline()
+    while row != "":
+        row = _clean(row)
+        for i in range(len(row)):
+            _db[fields[i]].append(row[i])
+        row = f.readline()
+
+    _db["weight"] = np.array([int(x) for x in _db["weight"]])
+    _db["std_weight"] = np.array([int(x) for x in _db["std_weight"]])
+    start_time = _times(_db['timestamp'][0])
+    _db["time"] = np.array([_times(x) - start_time for x in _db["timestamp"]])
+    f.close()
+    return _db
 
 
-db["weight"] = [int(x) for x in db["weight"]]
-db["std_weight"] = [int(x) for x in db["std_weight"]]
-start_time = times(db['timestamp'][0])
-db["time"] = [times(x) - start_time for x in db["timestamp"]]
+db = open_csv(path="/home/camila/Descargas/vacas.csv")
+diff = db["time"][1:]-db["time"][0:-1]
 
-#Graficar
+
+# Plot
 plt.plot(db["time"], db["weight"], "r.")
 plt.errorbar(db["time"], db["weight"], yerr=db["std_weight"], ls="")
 plt.xlabel("Time")
 plt.ylabel("Weight")
 plt.show()
 
+plt.plot(diff)
+plt.show()
 
-
-print(fields)
+print(len(db["time"]))
+print(diff)
 print(db["time"])
-f.close()
+
 
 
